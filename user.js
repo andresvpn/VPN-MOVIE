1081f9be8d5f074acaedfc48fd90b73b31e1f21b
andresvpn <125711121+andresvpn@users.noreply.github.com>
2024-05-27 17:47:12 -0500

    Add files via upload

--- user.js
+++ user.js
@@ -1,12 +1,8 @@
 const usuarios = [
-    { nombre: "1234", contraseña: "1234" },
-    { nombre: "andresvpn", contraseña: "1088829889" },
+    { id: 1, nombre: "1234", contraseña: "1234", fechaVencimiento: "2024-07-01" },
+    { id: 2, nombre: "andresvpn", contraseña: "1088829889", fechaVencimiento: "2024-04-24" },
     // Agrega más usuarios si es necesario
 ];
-
-// Verificar si existen usuarios en el Local Storage y cargarlos
-let usuariosGuardados = localStorage.getItem('usuarios');
-usuariosGuardados = usuariosGuardados ? JSON.parse(usuariosGuardados) : [];
 
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
@@ -17,10 +13,17 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     const usuario = usuarios.find(user => user.nombre === username && user.contraseña === password);
     if (usuario) {
-        errorMessage.textContent = ''; // Clear any previous error messages
-        setTimeout(() => {
-            window.location.href = 'index.html'
-        }, 1000);
+        const fechaVencimiento = new Date(usuario.fechaVencimiento);
+        const fechaActual = new Date();
+
+        if (fechaActual > fechaVencimiento) {
+            errorMessage.textContent = 'Usuario y contraseña han vencido';
+        } else {
+            errorMessage.textContent = ` bienvenid@ ${usuario.nombre}`; // Clear any previous error messages
+            setTimeout(() => {
+                window.location.href = 'index.html'
+            }, 3000);
+        }
     } else {
         errorMessage.textContent = 'Usuario o contraseña incorrectos';
     }
