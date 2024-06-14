9e48c9986c982fbecff506ddcf9d8ed9cf4f643e
andresvpn <125711121+andresvpn@users.noreply.github.com>
2024-06-13 23:41:50 -0500

    Create notificaciones.js

---
+++ notificaciones.js
@@ -0,0 +1,12 @@
+const notifications = [ 
+ { title: "JUNG_E", 
+message: "Pelicula Disponible",
+ time: "13/06/2024", 
+ imageUrl: "http://mastvgo.com:80/images/1CX9HZ1TpsR6jq_dGSnHkF7DaaYG-JnaH_5zbvTz1M5mB5g6BUIWNSoocDNYisyLD4w4im6plfFTjE8zCaB6xAPM8nCPeFYVg6A5Jf5ul6Q.jpg", 
+ url: "go:35" },
+{ title: "CHAPPIE", 
+message: "Pelicula Disponible",
+ time: "13/06/2024", 
+ imageUrl: "https://media.themoviedb.org/t/p/w220_and_h330_face/hpnuXlKKWznTgbheGe4iQXzkuwJ.jpg", 
+ url: "go:45" },
+        ];
