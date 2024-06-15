006da4335b60a7d4785d7289883e1f9f6c9c8407
andresvpn <125711121+andresvpn@users.noreply.github.com>
2024-06-15 09:47:21 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,9 @@
 const usuarios = [
     { id: 1, nombre: "1234", contraseña: "1234", fechaVencimiento: "2024-07-01", limite: 1, conexiones: { login: 0, home: 0, live: 0, series: 0, buscador: 0, notificaciones: 0 } },
     { id: 2, nombre: "andresvpn", contraseña: "1088829889", fechaVencimiento: "2024-04-24", limite: 2, conexiones: { login: 0, home: 0, live: 0, series: 0, buscador: 0, notificaciones: 0 } },
-    { id: 2, nombre: "alejandra", contraseña: "123", fechaVencimiento: "2024-012-24", limite: 2, conexiones: { login: 0, home: 0, live: 0, series: 0, buscador: 0, notificaciones: 0 } },
+    { id: 3, nombre: "alejandra", contraseña: "123", fechaVencimiento: "2024-012-24", limite: 2, conexiones: { login: 0, home: 0, live: 0, series: 0, buscador: 0, notificaciones: 0 } },
+    { id: 2, nombre: "esteban", contraseña: "123", fechaVencimiento: "2024-012-24", limite: 2, conexiones: { login: 0, home: 0, live: 0, series: 0, buscador: 0, notificaciones: 0 } },
+  
     // Agrega más usuarios si es necesario
 ];
 
